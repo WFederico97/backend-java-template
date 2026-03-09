bff04413929bd1506923a4b7a3dd0dc218084d78
WFederico97 <wuthrich09@gmail.com>
2026-03-08 22:52:00 -0300

    First commit

---
+++ src/main/java/wfederico/backendjavacoretemplate/BackendJavaCoreTemplateApplication.java
@@ -0,0 +1,13 @@
+package wfederico.backendjavacoretemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaCoreTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaCoreTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wfederico/backendjavacoretemplate/BackendJavaCoreTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package wfederico.backendjavacoretemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaCoreTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
