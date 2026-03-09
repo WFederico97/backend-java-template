93bb84b8572f350d01913193f81f22d5c7d199b9
WFederico97 <wuthrich09@gmail.com>
2026-03-09 19:59:55 -0300

    add: New BusinessLayerException file

--- src/main/java/wfederico/backendjavacoretemplate/BackendJavaCoreTemplateApplication.java
+++ src/main/java/wfederico/backendjavacoretemplate/BackendJavaCoreTemplateApplication.java
@@ -2,8 +2,10 @@ package wfederico.backendjavacoretemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendJavaCoreTemplateApplication {
 
     public static void main(String[] args) {
