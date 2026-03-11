0359587384d092ccdd6fa36b61154dca7fdf01a6
WFederico97 <wuthrich09@gmail.com>
2026-03-11 20:46:00 -0300

    add: Kafka configuration and application properties

---
+++ src/main/java/wfederico/backendjavacoretemplate/core/messaging/KafkaConfig.java
@@ -0,0 +1,54 @@
+package wfederico.backendjavacoretemplate.core.messaging;
+
+import com.fasterxml.jackson.databind.JsonSerializable;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.*;
+import tools.jackson.databind.ser.jdk.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
+
+    //---Producer config---//
+
+    @Bean
+    public ProducerFactory<String,Object> producerFactory(){
+        Map<String,Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        //String for topic, json for object
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    //---Consumer config---//
+
+    @Bean
+    public ConsumerFactory<String,Object> consumerFactory(){
+        Map<String,Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        //String for topic, json for object
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+}
